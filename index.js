73d671018d4c51e579f9c1a5854f1c12ec5e323e
shaoun11 <heyshaoun@gmail.com>
2023-11-08 23:46:14 +0600

    jwt-updated

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cookieParser = require('cookie-parser');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
-
-
-
-app.use(cors());
+app.use(cors({
+  origin:['https://foddy-resturent-project.netlify.app/'],
+  credentials: true
+}));
+app.use(cookieParser());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uzun1bo.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -20,51 +21,109 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+
+// middlewares
+const logger = async (req, res, next) => {
+  console.log('called:', req.host, req.originalUrl)
+  next();
+}
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+      return res.status(401).send({ message: 'Your access unauthorized ' })
+  }
+  jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+          return res.status(401).send({ message: ' Your access unauthorized ' })
+      }
+      req.user = decoded;
+      console.log("decoded-data",decoded);
+      next();
+  })
+}
+
+
+
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
 
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
 
     const database = client.db("fooddata").collection("allfoods");
     const orderdatabase = client.db("fooddata").collection("order");
     const userdatabase = client.db("fooddata").collection("userdata");
 
-
     app.get("/allfoods", async (req, res) => {
-     
-        let query={}
-         const name=req.query.foodName;
-        if (name) {
-            query.foodName=name
-        }
+      let query = {};
+      const name = req.query.foodName;
+      if (name) {
+        query.foodName = name;
+      }
 
-        const page=parseInt(req.query.page);
-        const limit=9;
-        const skip=(page)*limit;
+      const page = parseInt(req.query.page);
+      const limit = 9;
+      const skip = page * limit;
 
-        const result = await database.find(query).skip(skip).limit(limit).sort({order:"desc"}). toArray();
-        const total= await database.countDocuments();
-        res.send({result,total});
-      });
+      const result = await database
+        .find(query)
+        .skip(skip)
+        .limit(limit)
+        .sort({ order: "desc" })
+        .toArray();
+      const total = await database.countDocuments();
+      res.send({ result, total });
+    });
 
-      app.get("/foods/:_id",async(req,res)=>{
-        const id=req.params._id;
-        const query={
-            _id:new ObjectId(id)
-        }
-        const result=await database.findOne(query);
-        res.send(result);
-      })
+    app.get("/foods/:_id", async (req, res) => {
+      const id = req.params._id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await database.findOne(query);
+      res.send(result);
+    });
+    //get updated singledata
+    app.get("/addedfood/:_id", async (req, res) => {
+      const id = req.params._id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await database.findOne(query);
+      res.send(result);
+    });
 
+    //updated Add Food
+    app.put("/addedfood/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      console.log("id", id, data);
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedata = {
+        $set: {
+          foodName: data.foodName,
+          foodImage: data.foodImage,
+          foodCategory: data.foodCategory,
+          order: data.order,
+          price: data.price,
+          country: data.country,
+          description: data.description,
+        },
+      };
+      const result = await database.updateOne(filter, updatedata, options);
+      res.send(result);
+    });
 
     //add food data
 
-    app.post("/allfoods", async (req, res) => {
+    app.post("/allfoods", async logger,verifyToken (req, res) => {
       const order = req.body;
       const result = await database.insertOne(order);
       console.log(result);
@@ -74,18 +133,33 @@ async function run() {
     //add mongodb user data
 
     app.post("/user", async (req, res) => {
-      const user = req.body;
-      const result = await userdatabase.insertOne(user);
+      const userdata = req.body;
+      const result = await userdatabase.insertMany(userdata);
       console.log(result);
       res.send(result);
     });
+
+       //jwt post
+       app.post("/jwt",async(req,res)=>{
+        const user=req.body;
+        console.log('access-token',user);
+    const token= jwt.sign(user,secret,{ expiresIn: '10h' })
+  
+    res.cookie("token",token,{
+      httpOnly:true,
+      secure:true,
+      sameSite:"none"
+  
+    })
+    .send({success:true})
+    })
+  
 
     //get user data
     app.get("/user", async (req, res) => {
       const result = await userdatabase.find().toArray();
       res.send(result);
     });
-
 
     //get myadded data
     app.get("/addedfood", async (req, res) => {
@@ -101,47 +175,27 @@ async function run() {
       res.send(result);
     });
 
-     //get order data
-     app.get("/order", async (req, res) => {
+    //get order data
+    app.get("/order", async (req, res) => {
       const result = await orderdatabase.find().toArray();
       res.send(result);
     });
 
-    // //updated sell data
-    // app.put("/foods/:_id", async (req, res) => {
-    //   const id = req.params._id;
-    //   const data = req.body;
-    //   console.log("id", id, data);
-    //   const filter = { _id: new ObjectId(id) };
-    //   const options = { upsert: true };
-    //   const updatedata = {
-    //     $set: {
-    //       order:data.order
-    //     },
-        
-    //   };
-    //   const result = await database.updateOne(filter,updatedata,options);
-    //   console.log(result);
-    //   res.send(result);
-     
-    // });
-
-
-     //delete  orderd data 
-  app.delete("/order/:_id", async (req, res) => {
-    const id = req.params._id;
-    const query = {
-      _id: new ObjectId(id),
-    };
-    const result = await orderdatabase.deleteOne(query);
-    console.log(result);
-    res.send(result);
-  });
-
-      
+    //delete  orderd data
+    app.delete("/order/:_id", async (req, res) => {
+      const id = req.params._id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await orderdatabase.deleteOne(query);
+      console.log(result);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -149,15 +203,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
 app.get("/", (req, res) => {
-    res.send("Food Api...");
-  });
+  res.send("Food Api...");
+});
 
-  app.listen(port, () => {
-    console.log(`Food server is Running on port ${port}`);
-  });
-    
+app.listen(port, () => {
+  console.log(`Food server is Running on port ${port}`);
+});
